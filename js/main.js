120f65b3f0d40496cdc10ab18d51d14e2e414254
Ken Kotch <hello@ken.uno>
2017-08-15 20:54:43 -0600

    added remove item button

--- js/main.js
+++ js/main.js
@@ -1,94 +1,70 @@
 document.addEventListener('DOMContentLoaded', () => {
-// $(document).ready(function () {
 
-const food = {
-  burger: ['Royale with Cheese', '$9.37', 'img/burger.jpg'],
-  pizza: ['Wood Oven Pizza', '$13.21', 'img/pizza.jpg'],
-  ribs: ['Meaty Ribs with extra meat', '$16.99', 'img/ribs.jpg'],
-  iceCream: ['Melty Ice Cream', '$0.48', 'img/ice_cream.jpg']
-}
+  //variables
+  const food = {
+    burger: ['Royale with Cheese', '$9.37', 'img/burger.jpg'],
+    pizza: ['Wood Oven Pizza', '$13.21', 'img/pizza.jpg'],
+    ribs: ['Meaty Ribs with extra meat', '$16.99', 'img/ribs.jpg'],
+    iceCream: ['Melty Ice Cream', '$0.48', 'img/ice_cream.jpg']
+  }
+  let ol = document.getElementsByClassName('buying')[0]
+  let burger = document.getElementById('burger')
+  let pizza = document.getElementById('pizza')
+  let ribs = document.getElementById('ribs')
+  let iceCream = document.getElementById('iceCream')
 
-let ol = document.getElementsByClassName('buying')[0]
-let burger = document.getElementById('burger')
-let pizza = document.getElementById('pizza')
-let ribs = document.getElementById('ribs')
-let iceCream = document.getElementById('iceCream')
+  function addFood(ol, text) {
+    let li = document.createElement('li')
+    li.innerHTML = text
+    ol.appendChild(li)
+  }
 
-function addFood(ol, text) {
-  let li = document.createElement('li')
-  li.innerHTML = text
-  ol.appendChild(li)
-}
+  // add Burger
+  burger.addEventListener('click', function() {
+    addFood(ol, food.burger[0])
+  })
 
-// add Burger
-burger.addEventListener('click', function() {
-  addFood(ol, 'Royale with Cheese')
+  // add Pizza
+  pizza.addEventListener('click', function() {
+    addFood(ol, food.pizza[0])
+  })
+
+  // add Ribs
+  ribs.addEventListener('click', function() {
+    addFood(ol, food.ribs[0])
+  })
+
+  // add Ice Cream
+  iceCream.addEventListener('click', function() {
+    addFood(ol, food.iceCream[0])
+  })
+
+  //remove last food item
+  function removeLi() {
+    if (ol.lastChild) {
+      ol.lastChild.remove()
+    }
+  }
+  let rmButton = document.getElementById('removeBtn')
+  rmButton.addEventListener('click', function() {
+    removeLi()
+  })
 })
 
-// add Pizza
-pizza.addEventListener('click', function() {
-  addFood(ol, 'Wood Fired Pizza')
-})
-
-// add Ribs
-ribs.addEventListener('click', function() {
-  addFood(ol, 'Sweet, Sweet Ribs')
-})
-
-// add Ice Cream
-iceCream.addEventListener('click', function() {
-  addFood(ol, 'Melty Ice Cream')
-})
-
-
-
-
-})
-
-
-
-//
-//
-// function createLi(ol, text) { //create li, put it in ol
-//   let li = document.createElement('li')
-//   li.innerHTML = text
-//   ol.appendChild(li)
-// }
-//
-// function removeLi() { //remove 1 li
-//   // let ol = document.getElementsByClassName('kenList')[0]
-//   if (ol.lastChild){
-//   ol.lastChild.remove()
-//   }
-// }
-//
-// let button = document.getElementById('addBtn') //call createLi
-// button.addEventListener('click', function() {
-//   createLi(ol, 'some text')
+// $('#simple-form').on('submit', function (event) {
+//   const serialized = $('#simple-form').serializeArray()
+//   //this is how to add and print #s for ken delivers!
+//   let num01 = parseInt($('#num-01').val(), 10)
+//   let num02 = parseInt($('#num-02').val(), 10)
+//   let total = num01 + num02
+//   $('#total').text(`$${total}!`)
+//   console.log(serialized)
+//   // console.log(total)
+//   return false
 // })
 //
-// let rmButton = document.getElementById('removeBtn') //call removeLi
-// rmButton.addEventListener('click', function() {
-//   removeLi()
+// $('#yay-btn').on('click', function (event) {
+//   event.preventDefault()
+//   alert('Yeah!')
+//   console.log('YAY YAY YAY')
 // })
-
-
-
-  //
-  // $('#simple-form').on('submit', function (event) {
-  //   const serialized = $('#simple-form').serializeArray()
-  //   //this is how to add and print #s for ken delivers!
-  //   let num01 = parseInt($('#num-01').val(), 10)
-  //   let num02 = parseInt($('#num-02').val(), 10)
-  //   let total = num01 + num02
-  //   $('#total').text(`$${total}!`)
-  //   console.log(serialized)
-  //   // console.log(total)
-  //   return false
-  // })
-  //
-  // $('#yay-btn').on('click', function (event) {
-  //   event.preventDefault()
-  //   alert('Yeah!')
-  //   console.log('YAY YAY YAY')
-  // })
